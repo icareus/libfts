1a30cefba72897e18c34111c0dae1fb8d668018c
Antoine BARBAROUX <abarbaro@e1r3p9.42.fr>
2015-07-04 20:08:28 +0200

    Toupper, Tolower

---
+++ src/ft_tolower.s
@@ -0,0 +1,14 @@
+global _ft_tolower
+
+section .text
+
+_ft_tolower:
+	cmp		rdi, 'A'
+	jl		end
+	cmp		rdi, 'Z'
+	jg		end
+	add		rdi, 32
+
+end:
+	mov		rax, rdi
+	ret
---
+++ src/ft_toupper.s
@@ -0,0 +1,14 @@
+global _ft_toupper
+
+section .text
+
+_ft_toupper:
+	cmp		rdi, 'a'
+	jl		end
+	cmp		rdi, 'z'
+	jg		end
+	sub		rdi, 32
+
+end:
+	mov		rax, rdi
+	ret
