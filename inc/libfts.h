583552ae502c3f3c49bdf8bdef64ca137ab2e0ab
Antoine BARBAROUX <abarbaro@e1r3p9.42.fr>
2015-07-04 20:37:40 +0200

    updated hader

--- inc/libfts.h
+++ inc/libfts.h
@@ -16,6 +16,7 @@
 char	*ft_strdup(char *str);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
